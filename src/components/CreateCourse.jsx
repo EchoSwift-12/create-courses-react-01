e80d026a16bf604255c00786263cf93486c27933
[EchoSwift-12] <[amtulrahmansadiya@gmail.com]>
2024-07-07 13:22:49 +0530

    useState

--- src/components/CreateCourse.jsx
+++ src/components/CreateCourse.jsx
@@ -1,7 +1,25 @@
-const CreateCourse = () =>{
+import { useState } from "react"
 
-    return<>
-   <input type="text" placeholder="Enter Course Name"/>
+const CreateCourse = () => {
+
+    const [courseName, setcourseName] = useState("")
+    const [courseDescription, setcourseDescription] = useState("")
+
+    const changeCourseNameInput  = (e) =>{
+        setcourseName(e.target.value)
+        console.log(courseName)
+    }
+    const changeCourseDescriptionInput = (e) =>{
+        setcourseDescription(e.target.value)
+    }
+
+    return <>
+        <input type="text" placeholder="Enter Course Name"
+            onChange={changeCourseNameInput} />
+        <input type="text" placeholder="Enter Course Description" 
+        onChange={changeCourseDescriptionInput}/>   
+        <p>{courseName}</p>
+        <p>{courseDescription}</p>
     </>
 }
 
